dcc0f2ae36b9ede94457c11800091ef8f73b89e9
Casper Bollen <halcwb@gmail.com>
2022-08-16 08:21:05 +0200

    chore: changed docker to dotnet6

--- Dockerfile
+++ Dockerfile
@@ -1,33 +1,20 @@
-FROM mcr.microsoft.com/dotnet/aspnet:6.0 as build
+FROM mcr.microsoft.com/dotnet/sdk:6.0 as build
 
 # Install node
-RUN curl -sL https://deb.nodesource.com/setup_14.x | bash
+RUN curl -sL https://deb.nodesource.com/setup_18.x | bash
 RUN apt-get update && apt-get install -y nodejs
 
 WORKDIR /workspace
-COPY .config .config
+COPY . .
 RUN dotnet tool restore
-COPY .paket .paket
-COPY paket.dependencies paket.lock ./
 
-FROM build as server-build
-COPY src/Shared src/Shared
-COPY src/Server src/Server
+RUN npm install
+RUN dotnet fable src/Client --run webpack
 RUN cd src/Server && dotnet publish -c release -o ../../deploy
 
 
-FROM build as client-build
-COPY package.json package-lock.json ./
-RUN npm install
-COPY webpack.config.js ./
-COPY src/Shared src/Shared
-COPY src/Client src/Client
-RUN dotnet fable src/Client --run webpack
-
-
 FROM mcr.microsoft.com/dotnet/aspnet:6.0
-COPY --from=server-build /workspace/deploy /app
-COPY --from=client-build /workspace/deploy /app
+COPY --from=build /workspace/deploy /app
 WORKDIR /app
 EXPOSE 8085
 ENTRYPOINT [ "dotnet", "Server.dll" ]
--- src/Server/Server.fs
+++ src/Server/Server.fs
@@ -10,40 +10,48 @@ open Shared
 open Shared.Api
 
 
-let tryGetEnv key = 
+let tryGetEnv key =
     match Environment.GetEnvironmentVariable key with
-    | x when String.IsNullOrWhiteSpace x -> None 
+    | x when String.IsNullOrWhiteSpace x -> None
     | x -> Some x
 
 
 let port =
     "SERVER_PORT"
-    |> tryGetEnv |> Option.map uint16 |> Option.defaultValue 8085us
+    |> tryGetEnv
+    |> Option.map uint16
+    |> Option.defaultValue 8085us
 
 
-let publicPath = Path.GetFullPath "../Client/public"
+let publicPath =
+    Path.GetFullPath "../Client/public"
 
 
 let webApi =
-    Remoting.createApi()
+    Remoting.createApi ()
     |> Remoting.fromValue serverApi
     |> Remoting.withRouteBuilder routerPaths
     |> Remoting.buildHttpHandler
 
 
-let webApp = choose [ webApi; GET >=> text "GenInteractions App. Use localhost: 8080 for the GUI" ]
+let webApp =
+    choose [
+        webApi
+        GET
+        >=> text "GenPRES App. Use localhost: 8080 for the GUI"
+    ]
 
 
-
-let application = application {
-    url ("http://*:" + port.ToString() + "/")
-    use_router webApp
-    use_static publicPath
-    use_gzip
+let application =
+    application {
+        url ("http://*:" + port.ToString() + "/")
+        use_router webApp
+        use_static "public" //publicPath
+        use_gzip
     //use_iis
-    
+
     //service_config serviceConfig
     //host_config Env.configureHost
-}
+    }
 
 run application
